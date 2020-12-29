c9dc716e0f6a6a6993d5f10251fa06808adb45df
西野 <nisino333@gmail.com>
2020-12-29 19:20:41 +0900

    pug

--- webpack.config.js
+++ webpack.config.js
@@ -29,11 +29,25 @@ module.exports = {
                         loader: 'file-loader',
                         options: {
                             esModule: false,
-                            name: 'images/[name].[ext]',
+                            name: 'images/[folder]/[name].[ext]',
                         }
                     }
                 ]
-            }
+            },
+            {
+                test: /\.pug/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    },
+                    {
+                        loader: 'pug-html-loader',
+                        options: {
+                            pretty: true,
+                        }
+                    }
+                ]
+            },
         ]
     },
     plugins: [
@@ -41,7 +55,12 @@ module.exports = {
             filename: './css/main.css',
         }),
         new HtmlWebpackPlugin({
-            template: './src/templates/index.html',
+            template: './src/templates/index.pug',
+            filename: 'index.html',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/templates/access.pug',
+            filename: 'access.html',
         }),
         new CleanWebpackPlugin(),
     ]
